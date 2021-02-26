7cbbc95243116e40da1b89e9c0c8e4819e1488fa
laura garcia <taillefe@outlook.es>
2021-02-26 13:48:29 +0100

    .

---
+++ PSP_PracticaEncriptacion/src/AlmacenaClaveSecreta.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.security.*;
+import javax.crypto.*;
+public class AlmacenaClaveSecreta {
+
+	public static void main(String[] args) {
+		try {
+			KeyGenerator kg = KeyGenerator.getlnstance("AES");
+			kg.init (128);
+			//genera clave secreta
+			SecretKey clave = kg.generateKey();
+			ObjectOutputStream out = new ObjectOutputStream(
+			new FileOutputStream("Clave.secreta"));
+			out.writeObject(clave);
+			out.close ();
+		} catch (NoSuchAlgorithmException e) {e .printStackTrace (); }
+			catch (FileNotFoundException e) {e.printStackTrace();}
+			catch (IOException e) {e.printStackTrace();}
+	}//main
+}//..AlmacenaClaveSecreta
